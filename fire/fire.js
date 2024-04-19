0985f116ecefd5ed4d3870bf889095e1b2407e34
marinaheadway <headway2121@gmail.com>
2024-04-19 15:50:02 +0300

    first commit

---
+++ fire/fire.js
@@ -0,0 +1,34 @@
+const button = document.querySelector("#myButton");
+button.addEventListener("click", function() {
+    document.querySelector("#player").play();
+    document.querySelector("#myVideo").play();
+})
+
+const timer=5;
+let amoutTime = timer*60;
+
+function calculateTime(){
+    const countdown=document.querySelector("#countdown");
+    let minutes=Math.floor(amoutTime/60);
+    let seconds=amoutTime%60;
+
+    if (seconds<10){
+        seconds="0" + seconds;
+    }
+
+     countdown.textContent=`${minutes} : ${seconds}`;
+     amoutTime--;
+  
+
+    if (amoutTime <0){
+        stopTimer();
+        amoutTime=0;
+    }
+
+    function stopTimer(){
+        clearInterval(timerId);
+        document.querySelector("#player").pause();
+    }
+}
+
+let timerId=setInterval(calculateTime,1000);
